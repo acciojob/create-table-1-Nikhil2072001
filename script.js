edf5384872ebb980c0cefa66bb57ed3388468973
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-05-28 11:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+var row=3;
 function insert_Row() {
     //Write your code here
-  
+  var tr = document.createElement("tr");
+	var td = document.createElement("td");
+	td.innerText = `Row${row} cell1`;
+		var td11 = document.createElement("td");
+	td11.innerText = `Row${row} cell2`;
+	tr.appendChild(td);
+	tr.appendChild(td11);
+	document.getElementById('sampleTable').appendChild(tr);
+
+	//  var tr2 = document.createElement("tr");
+	// var td2 = document.createElement("td");
+	// td2.innerText = `Row${row} cell2`;
+	// var td21 = document.createElement("td");
+	// td21.innerText = `Row${row} cell2`;
+	// tr2.appendChild(td2);
+	// tr2.appendChild(td21);
+	// document.getElementById('sampleTable').appendChild(tr2);
+	row++;
   
 }
